dc70b834b8e6cae33be1a8412c6fc97a880e5d67
Nadine2016 <nspies@us.ibm.com>
2019-09-19 20:42:00 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,51 +1,50 @@
 variable "hostname" {
    default = "webapp1"
-   description = "The hostname for the instance, for example, webapp1."
+   description = "Enter a host name for your classic infrastructure virtual server instance. This host name is used with the `domain` to create the full URL for your classic infrastructure virtual server instance."
 }
 
 variable "domain" {
    default = "domain.dev"
-   description = "The domain for the virtual machine, for example, domain.dev."
+   description = "Enter the domain name that you want to assign to your classic infrastructure virtual server instance. This domain name is used with the `hostname` to create the full URL for your classic infrastructure virtual server instance."
 }
 
 variable "datacenter" {
    default = "dal13"
-   description = "The data center to create resources in, for example, dal13. To get a list of all data centers, run the `ic sl vs options` command."
+   description = "Enter the data center where you want to provision your classic infrastructure virtual server instance. For available data centers, run `ibmcloud sl vs options` and review the values in the 'datacenter' field."
 }
 
 variable "os-reference-code" {
    default = "CENTOS_LATEST_64"
-   description = "The code that is used to provision the computing instance. To view the available OS reference codes, log in to the [IBM Cloud Infrastructure (SoftLayer) API](https://api.softlayer.com/rest/v3/SoftLayer_Virtual_Guest_Block_Device_Template_Group/getVhdImportSoftwareDescriptions.json?objectMask=referenceCode)."
+   description = "Enter the reference code of the operating system that you want to install on your virtual server instance. To see available OS reference codes, log in to the [IBM Cloud Infrastructure (SoftLayer) API](https://api.softlayer.com/rest/v3/SoftLayer_Virtual_Guest_Block_Device_Template_Group/getVhdImportSoftwareDescriptions.json?objectMask=referenceCode)."
 }
 
 variable "flavor" {
    default = "C1_1X1X25"
-   description = "The flavor type of the virtual machine."
+   description = "Enter the flavor for your classic infrastructure virtual server instance. The flavor determines the type of virtual server instance that you want to create, including available CPU and memory. To find a list of supported flavors, run `ibmcloud sl vs options` and review the values in the 'flavors' field. For an overview of what is included in each flavor, see [Public virtual servers](https://cloud.ibm.com/docs/vsi?topic=virtual-servers-about-public-virtual-servers)."
 }
 
-
 variable "resource-group" {
-   description = "The resource group of the account where services are deployed." 
+   description = "Enter the name of the IBM Cloud resource group where you want to provision your database instance. To list available resource groups, run `ibmcloud resource groups`." 
 }
 
 variable "resource-instance-name" {
    default = "demo-postgres"
-   description = "The unique name for the database instance."
+   description = "Enter a unique name for your Databases for PostgreSQL service instance."
 }
 
 variable "database-service-name" {
    default = "databases-for-postgresql"
-   description = "The service name of the database."
+   description = "Enter the name of the database that you set up within your Databases for PostgreSQL service instance."
 }
 
 variable "database-service-plan" {
    default = "standard"
-   description = "The service plan of the database."   
+   description = "Enter the Databases for PostgreSQL service plan that you want."   
 }
 
 variable "location" {
    default = "eu-gb"
-   description = "The location in which to deploy the instance. The region and location must match."
+   description = "Enter the location, in which to deploy the database instance. The location that you choose must match the value in `region`. To find available locations, run `ibmcloud regions`."
 }
 
 variable "admin-password" {
@@ -54,19 +53,19 @@ variable "admin-password" {
 
 variable "members-memory-allocation-mb"{
    default = "3072"
-   description = "The memory size for the database that's split across all members."
+   description = "Enter the total amount of memory in megabytes that is shared between the database members within the database."
 }
 
 
 variable "members-disk-allocation-mb"{
    default = "20480"
-   description = "The disk size of the database that's split across all members."
+   description = "Enter the disk size in megabytes for the database that is shared across all database members."
 }
 
 
 variable "db-user-name"{
    default = "user123"
-   description = "The new database user name."
+   description = "Enter the user name of a database user that you want to set up. "
 }
 
 variable "db-user-password"{
